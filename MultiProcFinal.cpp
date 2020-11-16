291d9dad8297ddbbfc43dad1dcf0c072b11d60a0
Mario Roque <mm-roque@outlook.com>
2020-11-16 00:05:53 -0600

    Added Malloc verification
    This commit includes 
    -Verification to each matrix creation.

--- MultiProcFinal.cpp
+++ MultiProcFinal.cpp
@@ -3,19 +3,23 @@
 #include <string.h>
 #include <stdlib.h>
 
-/* TODO ( Completed [0/8] ):  
-    -Need to manage the case when im trying to read more than it currently exist on Matrix.txt.
-    -Need to perform serial mutliplication.
-    -Need to perform parallel multiplication 1 five times.
-    -Need to perform parallel multiplication 2 five times.
-    -Need to generate a write to txt function to save matrix C.
-    -Need to add time functions to calculate execution time.
-    -Need to generate a function compare matrixC.txt with Matrix results on serial processing.
-    -Need to add function descriptive comments.
+/* TODO ( Completed [1/9] ):  
+                            This list is not prioritized but it would be nice to follow the order.
+    [1]-Need to manage the case when im trying to read more than it currently exist on Matrix.txt.
+    [2]-Need to perform serial mutliplication.
+    [3]-Need to perform parallel multiplication 1 five times.
+    [4]-Need to perform parallel multiplication 2 five times.
+    [5]-Need to generate a write to txt function to save matrix C.
+    [6]-Need to add time functions to calculate execution time.
+    [7]-Need to generate a function compare matrixC.txt with Matrix results on serial processing.
+    [8]-Need to add a function that prints a table with the execution time for each run and processing type.
+    [9]-Need to add function descriptive comments.
+    [10]-Need to add a verification to manage when not possible to grant malloc space. [DONE]
 */
 
 //#define MatrixAPath "./Inputs/matrixA.txt"  //Apparently it doesnt like macros for constants
 //#define MatrixBPath "./Inputs/matrixB.txt"
+#define OUTOFMEMORY -1
 
 const char* MatrixAPath = "./Inputs/matrixA.txt";
 const char* MatrixBPath = "./Inputs/matrixB.txt";
@@ -48,11 +52,18 @@ void printMatrix(double* Matrix, size_t X, size_t Y) {
 
 }
 
+int mallocVerification(double* Matrix) { //https://stackoverflow.com/questions/763159/should-i-bother-detecting-oom-out-of-memory-errors-in-my-c-code
+    if (Matrix == NULL) {
+        return 0;
+    }
+    return 1;
+}
+
 
 int main()
 {
-    int AX, AY, BX, BY; //Variables for size.(AX,AY), * (BX,BY)
-
+    size_t AX, AY, BX, BY; //Variables for size.(AX,AY), * (BX,BY)
+   
     printf("Write the Matrix A size on the format (X,Y): ");
     scanf_s("%d %d", &AX, &AY);  //Why does it stop me from using scanf?
 
@@ -60,10 +71,24 @@ int main()
     scanf_s("%d %d", &BX, &BY);
 
     if (AX == BY) {
-        printf("Can calculate\n");
         //Creating the Matrix for A and B
         double* MatrixA = (double*)malloc(AX * AY * sizeof(double));
         double* MatrixB = (double*)malloc(BX * BY * sizeof(double));
+        double* MatrixC = (double*)malloc(AX * BY * sizeof(double)); //Result size row per column
+
+        //Allocation verification to avoid running out of memory.
+        if (!mallocVerification(MatrixA)){
+            fprintf(stderr, "FIRST OUT OF MEMORY OCURRED WHILE ALOCATING MATRIX: A... \nExiting with code %d", OUTOFMEMORY);
+            exit(OUTOFMEMORY);
+        }
+        if (!mallocVerification(MatrixB)) {
+            fprintf(stderr, "FIRST OUT OF MEMORY OCURRED WHILE ALOCATING MATRIX: B... \nExiting with code %d", OUTOFMEMORY);
+            exit(OUTOFMEMORY);
+        }
+        if (!mallocVerification(MatrixC)) {
+            fprintf(stderr, "FIRST OUT OF MEMORY OCURRED WHILE ALOCATING MATRIX: C... \nExiting with code %d", OUTOFMEMORY);
+            exit(OUTOFMEMORY);
+        }
 
         //Reading the A and B matrix
         readMatrix(MatrixAPath, MatrixA, AX, AY); //##Read Matrix A
